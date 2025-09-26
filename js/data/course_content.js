f0a24aa8f9faafda617b5077411023c4dda94657
Cgines-stech <cgines@stech.edu>
2025-09-26 14:40:25 -0600

    Update course_content.js

--- js/data/course_content.js
+++ js/data/course_content.js
@@ -5,24 +5,26 @@ export const COURSE_CONTENT = {
     credits: 3,
     description: "...",
     outline: "...",
-    textbooks: "...",
-    assignments: "Item 1",
+    textbooks: [
+      "Book 1",
+      "Book 2"
+    ],
+    assignments: [
+      "Item 1",
+      "Item 2"
+    ],
     hours: "M/W 10:00–11:15 AM (3 credits)",
-    campusRequirements: `... <a href="https://stech.edu/..." target="_blank">...</a>`
   },
-  // ...
+
   "IT 140": {
     description: "IT 140 description...",
     outline: "Modules A–F...",
     textbooks: "OS Concepts (10e), ISBN ...",
-    assignments: "Projects 60%\nLabs 20%\nReflections 20%",
+    assignments: [
+      "Projects 60%",
+      "Labs 20%",
+      "Reflections 20%"
+    ],
     hours: "T/Th 9:00–10:30 AM (3 credits)",
-    campusRequirements: `
-      InformaCast Statement: Southwest Tech uses InformaCast...
-      <a href="https://stech.edu/emergency-notifications/" target="_blank">
-        https://stech.edu/emergency-notifications/
-      </a>
-    `
-  },
-  // Add each remaining course here...
+  }
 };
