5a9e44afda9a8d7a7db489a699e631ea518d16c2
Cgines-stech <cgines@stech.edu>
2025-09-26 11:17:19 -0600

    Update campus_requirements.js

--- js/data/campus_requirements.js
+++ js/data/campus_requirements.js
@@ -1,5 +1,26 @@
 export const CAMPUS_REQUIREMENTS = [
-{ id:"campus_gen", label:"General Statement", body:"Southwest Tech requires..." },
-{ id:"campus_titleix", label:"Title IX", body:"Title IX statement..." },
-{ id:"campus_disability", label:"Disability Services", body:"If you have a documented disability..." },
+  {
+    id: "campus_gen",
+    label: "General Statement",
+    body: `
+      InformaCast Statement: Southwest Tech uses InformaCast to ensure the safety and well-being of our students.
+      In times of emergency, such as weather closures and delays, this app allows us to promptly deliver notifications
+      directly to your mobile devices. To stay informed and receive real-time updates, we encourage all students to sign up
+      for notifications. Your safety is our priority, and staying connected ensures a swift response to any unforeseen
+      circumstances. More information and directions for signing up are available at:
+      <a href="https://stech.edu/emergency-notifications/" target="_blank">
+        https://stech.edu/emergency-notifications/
+      </a>
+    `
+  },
+  {
+    id: "campus_titleix",
+    label: "Title IX",
+    body: "Title IX statement..."
+  },
+  {
+    id: "campus_disability",
+    label: "Disability Services",
+    body: "If you have a documented disability..."
+  }
 ];
